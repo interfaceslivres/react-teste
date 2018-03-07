a63673c8df2e86393d6051743d32297688fa36bc
Interfaces Livres <paulohsms@gmail.com>
2018-03-07 11:04:11 -0300

    filtro aplicado as horas e numero de playlists

--- src/App.js
+++ src/App.js
@@ -120,6 +120,11 @@ class App extends Component {
   }
 
   render() {
+    let playlistsToRender = this.state.serverData.user ? this.state.serverData.user.playlists
+    .filter(playlist =>
+      playlist.name.toLowerCase().includes(
+        this.state.filterString.toLowerCase())
+    ) : []
     return (
           <div className="App">
             {this.state.serverData.user ?
@@ -127,16 +132,13 @@ class App extends Component {
                 <h1 style={{...defaultStyle, "font-size": "54px"}}>
                  {this.state.serverData.user.name}s Playlist
                 </h1>
-                  <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
-                  <HoursCounter playlists={this.state.serverData.user.playlists}/>
+                  <PlaylistCounter playlists={playlistsToRender}/>
+                  <HoursCounter playlists={playlistsToRender}/>
                 <Filter onTextChange={text => {
                 this.setState({filterString: text})
                 }
                 }/>
-               {this.state.serverData.user.playlists.filter(playlist =>
-                 playlist.name.toLowerCase().includes(
-                   this.state.filterString.toLowerCase())
-               ).map(playlist =>
+               {playlistsToRender.map(playlist =>
                     <Playlist playlist={playlist} />
                   )}
 
